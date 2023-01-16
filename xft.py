ddda97d0ba12120a3fdf44f7b4e6b6e440a8a7d9
ABHISHEKOO <110409990+ABHISHEKOO@users.noreply.github.com>
2023-01-16 20:55:41 +0530

    co

--- xft.py
+++ xft.py
@@ -1,36 +1,41 @@
 #input loop
 r=int(input('enter the row no'))
 c=int(input('enter column no'))
-a=[]
+a=[ ]
 for i in range (r):
-    t=[]
+    t=[ ]
     for j in range (c):
         v=int(input('enter no'))
         t.append (v)
     a.append(t)
-b=[]
+print(a)
+b=[ ]
 for i in range(r):
-    t=[]
+    t=[ ]
     for j in range(c):
         v=int(input('enter no'))
         t.append(v)
-    b.append(t)
+    b.append (t)
+print(b)
 # processing loop.
 s=[]
 for i in range (r):
     t=[]
     for j in range (c):
-        v=a[i] [j]+b[i] [j]
-        t.append (v)
-    s.append (t)
+        t.append
+    s.append
+for i in range (len(a)):
+    for j in range (len(s)):
+        for k in range (len(b)):
+            s[i][j]+=a[i][j]*b[i][j]
 # output loop.
 for i in range (r):
     for j in range (c):
         print(a[i] [j],end=" ")
-    print("  ", '+' , "  ",end=" ")
+    print("  ", '+' , "  ")
     for j in range (c):
-        print(b[i] [j],end=" ")
-    print("  " , '=' , "  ",end=" ")
+        print(b[i][j],end=" ")
+    print("  " , '=' , "  ")
     for j in range (c):
-        print(s[i] [j], end = " ")
+        print(s[i][j], end = " ")
     print()
