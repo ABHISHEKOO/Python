bf490c0cc9f99c0121889430cc4947aa8d89c294
ABHISHEKOO <110409990+ABHISHEKOO@users.noreply.github.com>
2023-02-07 08:21:31 +0530

    suum

---
+++ sum.py
@@ -0,0 +1,10 @@
+num=eval(input("enter four-digit number :"))
+print("entered number is:",num)
+r1=num%10
+q1=num//10
+r2=q1%10
+q2=q1//10
+r3=q2%10
+q3=q2//10
+r4=q3%10
+print('reverse of',num,'is:',r1,r2,r3,r4)
