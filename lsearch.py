3bb2eb00db93c3cd2b2fb05286a9c47eec9fb9b4
ABHISHEKOO <110409990+ABHISHEKOO@users.noreply.github.com>
2023-03-01 20:13:39 +0530

    lsearch

---
+++ lsearch.py
@@ -0,0 +1,18 @@
+def lsearch(l,n,key):
+    for i in range(0,n):
+        if (l[i]==key):
+            return i
+        return -1
+    
+n=int(input('enter size of list: '))
+l=[]
+for i in range(n):
+    x=int(input('enter list element :'))
+    l.append(x)
+print(l)
+key=int(input('enter key :'))
+r=lsearch(l,n,key)
+if r==-1:
+    print("key is not foud")
+else:
+    print("key is found")
